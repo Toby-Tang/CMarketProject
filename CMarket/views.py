c822843e73fcd97a3e31756bbe5552ae2f915f2e
cofftea <l.lu9928@gmail.com>
2021-04-19 22:48:44 -0400

    Hi there

--- CMarket/views.py
+++ CMarket/views.py
@@ -1,9 +1,6 @@
-from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render, redirect
-from django.urls import reverse
 from django.contrib.auth.models import User, auth
-
-from .models import Question, Choice, Message, Post
+from .models import Message, Post
 
 
 def home(request):
@@ -56,45 +53,21 @@ def post(request):
         else:
             try:
                 Message.objects.create(target=target, content=content, origin=origin)
-                return render(request, "CMarket/post.html", {"status": "Message successful sent!"})
+                return render(request, "CMarket/post.html", {"status": "Message successfully sent!"})
             except:
                 return render(request, "CMarket/post.html", {"status": "An expected error has occurred."})
 
 
 def public(request):
-    return render(request, "CMarket/public.html")
+    posts = Post.objects.all()
+    context = {"posts": posts}
+    return render(request, "CMarket/public.html", context)
 
 
 def private(request):
-    return render(request, "CMarket/private.html")
-
-
-def index(request):
-    latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    context = {"latest_question_list": latest_question_list}
-    return render(request, "CMarket/index.html", context)
-
-
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "CMarket/detail.html", {"question": question})
-
-
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "CMarket/results.html", {"question": question})
-
-
-def vote(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    try:
-        selected_choice = question.choice_set.get(pk=request.POST["choice"])
-    except (Keydisp, Choice.DoesNotExist):
-        return render(request, "CMarket/detail.html", {
-            "question": question,
-            "disp_message": "You didn't select a choice.",
-        })
-    else:
-        selected_choice.votes += 1
-        selected_choice.save()
-        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
+    if not request.user.is_authenticated:
+        return redirect("/login")
+    temp = Message.objects.all()
+    messages = [i for i in temp if i.target == request.user.username]
+    context = {"messages": messages}
+    return render(request, "CMarket/private.html", context)
--- CMarketProject/urls.py
+++ CMarketProject/urls.py
@@ -5,7 +5,7 @@ from django.contrib import admin
 from CMarket import views
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    #url(r'^admin/', admin.site.urls),
     url(r'^$', views.home, name='home'),
     url(r'^login/', views.login, name='login'),
     url(r'^post/', views.post, name='post'),
